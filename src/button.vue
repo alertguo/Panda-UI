61cc776785080e3d07e7eab58a2f0466546ad288
alertguo <1196614612@qq.com>
2021-04-01 17:59:16 +0800

    引入svg，初步实现一个按钮的icon

--- src/button.vue
+++ src/button.vue
@@ -1,29 +1,36 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button">
+    <svg class="icon">
+      <use xlink:href="#i-setting"></use>
+    </svg>
+    <slot></slot>
+  </button>
 </template>
 
 <script>
-  export default {
-  }
+export default {}
 </script>
 
 <style lang="scss" scoped>
-  .g-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    &:hover {
-      background-color: var(--border-color-hover);
-    }
-    &:active {
-      background-color: var(--button-active-bg);
-    }
-    /*去掉点击时的蓝色框*/
-    &:focus {
-      outline: none;
-    }
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+
+  &:hover {
+    background-color: var(--border-color-hover);
   }
+
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+
+  /*去掉点击时的蓝色框*/
+  &:focus {
+    outline: none;
+  }
+}
 </style>
