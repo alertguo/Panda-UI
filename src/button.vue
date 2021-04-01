40cad1bb76a191de7680400c4af1bbb65fcbb3d1
alertguo <1196614612@qq.com>
2021-04-01 16:26:28 +0800

    将 botton 组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="scss" scoped>
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      background-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    /*去掉点击时的蓝色框*/
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
