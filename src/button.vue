d02cf8d6e2ca9d4e4451c86ed4afc4d12b969728
alertguo <1196614612@qq.com>
2021-04-02 14:17:19 +0800

    初步添加 loading 标签并添加动画

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
   <button :class="{[`icon-${iconPosition}`]:true}" class="g-button">
     <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
     <div class="content">
       <slot/>
     </div>
@@ -23,6 +24,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@keyframes spin {
+  0% {
+    transform: rotate(0deg)
+  }
+  100% {
+    transform: rotate(360deg)
+  }
+}
+
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
@@ -67,6 +77,9 @@ export default {
     > .content {
       order: 1;
     }
+  }
+  > .loading{
+    animation: spin 2s infinite linear;
   }
 }
 </style>
