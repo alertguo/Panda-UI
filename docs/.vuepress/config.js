e3e3ab576182d663fffead080f24d96c92be510b
alertguo <1196614612@qq.com>
2021-04-06 11:29:18 +0800

    初步添加导航和侧边栏

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: '熊猫 UI',
+  description: '一个好用的 UI ',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: [
+      '/install/',
+      '/get-started/'
+    ],
+  }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,4 @@
-# Hello VuePress
+# Panda UI 官网
+
+熊猫 UI 是一个好用的 UI 框架，提供了 Button
+...... 等常用组件，适合移动端和 PC 端使用
