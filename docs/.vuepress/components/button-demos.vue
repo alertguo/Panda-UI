62e1ad9986046ccf7599159233b799549fec49fe
alertguo <1196614612@qq.com>
2021-04-06 18:55:53 +0800

    添加其他按钮，icon 不显示

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <g-button>默认按钮</g-button>
+    <g-button icon="setting">默认按钮</g-button>
+    <g-button icon="setting" icon-position="right">默认按钮</g-button>
+    <g-button :loading="true">默认按钮</g-button>
+    <g-button disabled>默认按钮</g-button>
   </div>
 </template>
 <script>
