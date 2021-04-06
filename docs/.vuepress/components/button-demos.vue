a6c5a8dadbfdc9b644b652efc0d62df9fcd7950e
alertguo <1196614612@qq.com>
2021-04-06 21:49:50 +0800

    添加代码显示，并使用正则修改间隙

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -5,13 +5,30 @@
     <g-button icon="setting" icon-position="right">默认按钮</g-button>
     <g-button :loading="true">默认按钮</g-button>
     <g-button disabled>默认按钮</g-button>
+    <g-button icon="left">默认按钮</g-button>
+    <g-button icon="right" icon-position="right">默认按钮</g-button>
+    <pre><code>{{ content }}</code></pre>
   </div>
 </template>
 <script>
 import Button from '../../../src/button'
+
 export default {
   components: {
     'g-button': Button,
+  },
+  data() {
+    return {
+      content: `
+      <g-button>默认按钮</g-button>
+      <g-button icon="setting">默认按钮</g-button>
+      <g-button icon="setting" icon-position="right">默认按钮</g-button>
+      <g-button :loading="true">默认按钮</g-button>
+      <g-button disabled>默认按钮</g-button>
+      <g-button icon="left">默认按钮</g-button>
+      <g-button icon="right" icon-position="right">默认按钮</g-button>
+      `.replace(/\t+| +/g, '').trim()
+    }
   }
 }
 </script>
