0e617b1b82e812cf483c5a75fc128732d9034d25
alertguo <1196614612@qq.com>
2021-04-06 18:12:27 +0800

    修改 style 不使用 var 使用 $ 来写 css

--- src/button.vue
+++ src/button.vue
@@ -10,6 +10,7 @@
 
 <script>
 import Icon from './icon'
+
 export default {
   components: {
     'g-icon': Icon
@@ -32,6 +33,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$font-size: 14px;
+$button-height: 32px;
+$button-bg: white;
+$button-active-bg: #eee;
+$border-radius: 4px;
+$color: #333;
+$border-color: #999;
+$border-color-hover: #666;
 @keyframes spin {
   0% {
     transform: rotate(0deg)
@@ -42,12 +51,12 @@ export default {
 }
 
 .g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
+  font-size: $font-size;
+  height: $button-height;
   padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
+  border-radius: $border-radius;
+  border: 1px solid $border-color;
+  background: $button-bg;
   display: inline-flex;
   justify-content: center;
   align-items: center;
@@ -55,11 +64,11 @@ export default {
   vertical-align: middle;
 
   &:hover {
-    background-color: var(--border-color-hover);
+    border-color: $border-color-hover;
   }
 
   &:active {
-    background-color: var(--button-active-bg);
+    background-color: $button-active-bg;
   }
 
   /*去掉点击时的蓝色框*/
