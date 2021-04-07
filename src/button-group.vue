99f45cced86290b2268b157458ff87fc326c6587
alertguo <1196614612@qq.com>
2021-04-07 12:11:30 +0800

    修改 var() 样式为 $

--- src/button-group.vue
+++ src/button-group.vue
@@ -7,6 +7,8 @@
 
 </script>
 <style lang="scss">
+$border-radius: 4px;
+
 .g-button-group {
   display: inline-flex;
   vertical-align: middle;
@@ -19,13 +21,13 @@
     }
 
     &:first-child {
-      border-top-left-radius: var(--border-radius);
-      border-bottom-left-radius: var(--border-radius);
+      border-top-left-radius: $border-radius;
+      border-bottom-left-radius: $border-radius;
     }
 
     &:last-child {
-      border-top-right-radius: var(--border-radius);
-      border-bottom-right-radius: var(--border-radius);
+      border-top-right-radius: $border-radius;
+      border-bottom-right-radius: $border-radius;
     }
 
     &:hover {
