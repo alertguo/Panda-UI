242c3e5941ea5f81c7672a24a1e2ae72b6bf46e7
alertguo <1196614612@qq.com>
2021-04-07 10:40:18 +0800

    更新正则的使用，修复代码显示没有空隙的问题

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <h3>预览：</h3>
+    <p>
+      <strong>预览</strong>
+    </p>
     <g-button>默认按钮</g-button>
     <g-button icon="setting">默认按钮</g-button>
     <g-button icon="setting" icon-position="right">默认按钮</g-button>
     <g-button :loading="true">默认按钮</g-button>
     <g-button disabled>默认按钮</g-button>
-    <h3>代码：</h3>
+    <p>
+      <strong>代码</strong>
+    </p>
     <pre><code>{{ content }}</code></pre>
   </div>
 </template>
@@ -20,12 +24,12 @@ export default {
   data() {
     return {
       content: `
-      <g-button>默认按钮</g-button>
-      <g-button icon="setting">默认按钮</g-button>
-      <g-button icon="setting" icon-position="right">默认按钮</g-button>
-      <g-button :loading="true">默认按钮</g-button>
-      <g-button disabled>默认按钮</g-button>
-      `.replace(/\t+| +/g, '').trim()
+        <g-button>默认按钮</g-button>
+        <g-button icon="setting">默认按钮</g-button>
+        <g-button icon="setting" icon-position="right">默认按钮</g-button>
+        <g-button :loading="true">默认按钮</g-button>
+        <g-button disabled>默认按钮</g-button>
+      `.replace(/^ {8}/gm, '').trim()
     }
   }
 }
