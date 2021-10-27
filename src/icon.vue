da8dfd0dc0298dbcaed2a87aeeb995a3586722cb
alertguo <1196614612@qq.com>
2021-10-27 20:20:37 +0800

    添加icon标签类型备注

--- src/icon.vue
+++ src/icon.vue
@@ -11,6 +11,14 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+//i-panda 熊猫头
+//i-loading 加载
+//i-download  下载
+//i-setting 设置
+//i-down 下
+//i-left 左
+//i-thumbs-up 点赞
+//i-right 右
 .g-icon {
   width: 1em;
   height: 1em;
