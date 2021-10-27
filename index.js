9a999cce1edd1a2bc545521a0a75456d41fedf1d
alertguo <1196614612@qq.com>
2021-10-27 22:32:26 +0800

    npm 包上传成功

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Button from './src/button'
 import ButtonGroup from './src/button-group'
 import Icon from './src/icon'
+import Input from './src/input'
 
 
-export {Button, ButtonGroup, Icon}
+export {Button, ButtonGroup, Icon,Input}
